de5116c94e129801c8e4a08f9129030aae8c8c41
Tsitohaina Rakotondramasy <tsitohaina.toetra.rak@gmail.com>
2022-10-18 13:43:58 -0400

    Updated frontend env

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
-/* @TODO replace with your variables
+/* @OK replace with your variables
  * ensure all variables on this page match your project
  */
 
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
+    url: 'tsitokely.us', // the auth0 domain prefix
+    audience: 'Coffee-Shop', // the audience set for the auth0 app
+    clientId: 'Nww3Snu0GAaSwoMPg6nH6ovBFmY1GQpk', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
